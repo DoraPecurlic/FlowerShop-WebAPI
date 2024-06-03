19eb78ac66951c7d7963d59423f24e9ce4e782c1
DoraPecurlic <dora.pecurlic01@gmail.com>
2024-06-03 14:01:36 +0200

    added post method "Order"

--- FlowerShop/FlowerShop.WebAPI/Controllers/OrderController.cs
+++ FlowerShop/FlowerShop.WebAPI/Controllers/OrderController.cs
@@ -1,187 +1,231 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+using FlowerShop.WebAPI.Models;
 
 namespace FlowerShop.WebAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+
     public class OrderController : ControllerBase
     {
-        
-        private static List<Order> orders = new List<Order>
-        {
-            new Order("Rose", 5, "Bouquet"),
-            new Order("Tulips", 7, "Flower Basket"),
-            new Order("Roses", 10, "Flower Box"),
-        };
+       
 
-        private readonly ILogger<OrderController> _logger;
-
-        public OrderController(ILogger<OrderController> logger)
+        public OrderController()
         {
-            _logger = logger;
         }
 
 
+
+        
         [HttpPost(Name = "Order")]
-        public HttpResponseMessage Order([FromBody] Order order)
+        public IActionResult Order([FromBody] Order order)
         {
-            if (order == null)
-            {
-                return new HttpResponseMessage(HttpStatusCode.BadRequest) { Content = new StringContent("Order data cannot be null.") };
-            }
             try
             {
-                // Business handling
-                bool isProcessed = ProcessOrder(order);
-                if (isProcessed == false)
+
+                string connectionString = WebApplication.Create().Configuration.GetConnectionString("DefaultConnection");
+
+
+
+                using (var connection = new NpgsqlConnection(connectionString))
                 {
-                    return new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                    connection.Open();
+
+                    using (var command = new NpgsqlCommand())
                     {
-                        Content = new StringContent("Order processing faild.")
-                    };
-                }
+                        command.Connection = connection;
+                        command.CommandText = @"
+                            INSERT INTO ""Order"" (""FlowerType"", ""Quantity"", ""OrderTypeId"", ""IsActive"", ""DateCreated"", ""DateUpdated"", ""CreatedByUserId"", ""UpdatedByUserId"")
+                            VALUES (@FlowerType, @Quantity, @OrderTypeId, @IsActive, @DateCreated, @DateUpdated, @CreatedByUserId, @UpdatedByUserId)";
 
-                orders.Add(order);
-                return new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = new StringContent("Succesfully ordered! Thank you for ordering .")
-                };
+                        command.Parameters.AddWithValue("FlowerType", order.FlowerType);
+                        command.Parameters.AddWithValue("Quantity", order.Quantity);
+                        command.Parameters.AddWithValue("OrderTypeId", order.OrderTypeId);
+                        command.Parameters.AddWithValue("IsActive", order.IsActive);
+                        command.Parameters.AddWithValue("DateCreated", order.DateCreated);
+                        command.Parameters.AddWithValue("DateUpdated", order.DateUpdated);
+                        command.Parameters.AddWithValue("CreatedByUserId", order.CreatedByUserId);
+                        command.Parameters.AddWithValue("UpdatedByUserId", order.UpdatedByUserId);
 
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Exception occurred: {ex.Message}");
-                return new HttpResponseMessage(HttpStatusCode.InternalServerError)
-                {
-                    Content = new StringContent("An error occurred while placing the order")
-                };
-            }
-
-        }
-
-        private bool ProcessOrder(Order order)
-        {
-            if (order.TypeOfFlower == null || order.OrderType == null || order.Quantity <= 0)
-            {
-                return false;
-            }
-            return true;
-        }
-
-
-
-        [HttpGet(Name = "SeeOrders")]
-        public IActionResult SeeOrders()
-        {
-
-            try
-            {
-                List<string> displayOrders = new List<string>();
-                foreach (var order in orders)
-                {
-                    displayOrders.Add($"Flower type: {order.TypeOfFlower}, Quantity: {order.Quantity}, Order type: {order.OrderType}");
-                }
-                return Ok(displayOrders.ToArray());
-            }
-            catch
-            {
-                return BadRequest();
-            }
-
-        }
-        
-
-        [HttpDelete(Name = "DeleteOrder")]
-        public HttpResponseMessage DeleteOrder([FromBody] Order order)
-        {
-            if (order == null)
-            {
-                return new HttpResponseMessage(HttpStatusCode.BadRequest) { Content = new StringContent("Order data cannot be null.") };
-            }
-            try
-            {
-                // Business handling
-                bool isProcessed = ProcessOrder(order);
-                if (isProcessed == false)
-                {
-                    return new HttpResponseMessage(HttpStatusCode.InternalServerError)
-                    {
-                        Content = new StringContent("Order deleting faild.")
-                    };
-                }
-
-                int indexToRemove = -1;
-                for (int i = 0; i < orders.Count; i++)
-                {
-                    if (orders[i].TypeOfFlower == order.TypeOfFlower && orders[i].Quantity == order.Quantity && orders[i].OrderType == order.OrderType)
-                    {
-                        indexToRemove = i;
-                        break;
+                        command.ExecuteNonQuery();
                     }
+                   
                 }
-
-                if (indexToRemove != -1)
+                if(order == null)
                 {
-                    orders.RemoveAt(indexToRemove);
-                    return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Order deleted successfully.") };
+                    return BadRequest();
                 }
-                return new HttpResponseMessage(HttpStatusCode.NotFound);
 
+                return Ok();
             }
             catch (Exception ex)
             {
-                _logger.LogError($"Exception occurred: {ex.Message}");
-                return new HttpResponseMessage(HttpStatusCode.InternalServerError)
-                {
-                    Content = new StringContent("An error occurred while deleting the order.")
-                };
+                Console.WriteLine($"Connection failed: {ex.Message}");
+                return BadRequest(ex.Message);
             }
-
         }
 
+        /* [HttpPost(Name = "Order")]
+         public HttpResponseMessage Order([FromBody] Order order)
+         {
+             if (order == null)
+             {
+                 return new HttpResponseMessage(HttpStatusCode.BadRequest) { Content = new StringContent("Order data cannot be null.") };
+             }
+             try
+             {
+                 // Business handling
+                 bool isProcessed = ProcessOrder(order);
+                 if (isProcessed == false)
+                 {
+                     return new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                     {
+                         Content = new StringContent("Order processing faild.")
+                     };
+                 }
 
-        [HttpPut(Name = "UpdateOrder")]
-        public HttpResponseMessage UpdateOrder([FromBody] Order order, [FromQuery] int newQuantity)
-        {
-            if (order == null)
-            {
-                return new HttpResponseMessage(HttpStatusCode.BadRequest) { Content = new StringContent("Order data cannot be null.") };
-            }
-            try
-            {
-                // Business handling
-                bool isProcessed = ProcessOrder(order);
-                if (isProcessed == false)
-                {
-                    return new HttpResponseMessage(HttpStatusCode.InternalServerError)
-                    {
-                        Content = new StringContent("Order updating faild.")
-                    };
-                }
+                 orders.Add(order);
+                 return new HttpResponseMessage(HttpStatusCode.OK)
+                 {
+                     Content = new StringContent("Succesfully ordered! Thank you for ordering .")
+                 };
+
+             }
+             catch (Exception ex)
+             {
+                 _logger.LogError($"Exception occurred: {ex.Message}");
+                 return new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                 {
+                     Content = new StringContent("An error occurred while placing the order")
+                 };
+             }
+
+         }
+
+         private bool ProcessOrder(Order order)
+         {
+             if (order.FlowerType == null || order.OrderTypeId == null || order.Quantity <= 0)
+             {
+                 return false;
+             }
+             return true;
+         }
 
 
-                for (int i = 0; i < orders.Count; i++)
-                {
-                    if (orders[i].TypeOfFlower == order.TypeOfFlower && orders[i].Quantity == order.Quantity && orders[i].OrderType == order.OrderType)
-                    {
-                        orders[i].Quantity = newQuantity;
-                        return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Order updated successfully.") };
-                    }
-                }
-                return new HttpResponseMessage(HttpStatusCode.NotFound);
 
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Exception occurred: {ex.Message}");
-                return new HttpResponseMessage(HttpStatusCode.InternalServerError)
-                {
-                    Content = new StringContent("An error occurred while deleting the order.")
-                };
-            }
+         [HttpGet(Name = "SeeOrders")]
+         public IActionResult SeeOrders()
+         {
 
-        }
+             try
+             {
+                 List<string> displayOrders = new List<string>();
+                 foreach (var order in orders)
+                 {
+                     displayOrders.Add($"Flower type: {order.FlowerType}, Quantity: {order.Quantity}, Order type: {order.OrderTypeId}");
+                 }
+                 return Ok(displayOrders.ToArray());
+             }
+             catch
+             {
+                 return BadRequest();
+             }
+
+         }
+
+
+         [HttpDelete(Name = "DeleteOrder")]
+         public HttpResponseMessage DeleteOrder([FromBody] Order order)
+         {
+             if (order == null)
+             {
+                 return new HttpResponseMessage(HttpStatusCode.BadRequest) { Content = new StringContent("Order data cannot be null.") };
+             }
+             try
+             {
+                 // Business handling
+                 bool isProcessed = ProcessOrder(order);
+                 if (isProcessed == false)
+                 {
+                     return new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                     {
+                         Content = new StringContent("Order deleting faild.")
+                     };
+                 }
+
+                 int indexToRemove = -1;
+                 for (int i = 0; i < orders.Count; i++)
+                 {
+                     if (orders[i].FlowerType == order.FlowerType && orders[i].Quantity == order.Quantity && orders[i].OrderTypeId == order.OrderTypeId)
+                     {
+                         indexToRemove = i;
+                         break;
+                     }
+                 }
+
+                 if (indexToRemove != -1)
+                 {
+                     orders.RemoveAt(indexToRemove);
+                     return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Order deleted successfully.") };
+                 }
+                 return new HttpResponseMessage(HttpStatusCode.NotFound);
+
+             }
+             catch (Exception ex)
+             {
+                 _logger.LogError($"Exception occurred: {ex.Message}");
+                 return new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                 {
+                     Content = new StringContent("An error occurred while deleting the order.")
+                 };
+             }
+
+         }
+
+
+         [HttpPut(Name = "UpdateOrder")]
+         public HttpResponseMessage UpdateOrder([FromBody] Order order, [FromQuery] int newQuantity)
+         {
+             if (order == null)
+             {
+                 return new HttpResponseMessage(HttpStatusCode.BadRequest) { Content = new StringContent("Order data cannot be null.") };
+             }
+             try
+             {
+                 // Business handling
+                 bool isProcessed = ProcessOrder(order);
+                 if (isProcessed == false)
+                 {
+                     return new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                     {
+                         Content = new StringContent("Order updating faild.")
+                     };
+                 }
+
+
+                 for (int i = 0; i < orders.Count; i++)
+                 {
+                     if (orders[i].FlowerType == order.FlowerType && orders[i].Quantity == order.Quantity && orders[i].OrderTypeId == order.OrderTypeId)
+                     {
+                         orders[i].Quantity = newQuantity;
+                         return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Order updated successfully.") };
+                     }
+                 }
+                 return new HttpResponseMessage(HttpStatusCode.NotFound);
+
+             }
+             catch (Exception ex)
+             {
+                 _logger.LogError($"Exception occurred: {ex.Message}");
+                 return new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                 {
+                     Content = new StringContent("An error occurred while deleting the order.")
+                 };
+             }
+
+         }*/
 
 
 
